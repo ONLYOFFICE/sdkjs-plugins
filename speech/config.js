3099ad642a17cb5c5a51cbdf12522f77d12d2cae
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-05-19 13:20:18 +0300

    no message

--- speech/config.js
+++ speech/config.js
@@ -24,13 +24,3 @@ window.Asc.plugin = {
         }
     ]
 };
-
-var plugin4                 = new Asc.CPlugin();
-plugin4.name                = "speech";
-plugin4.guid                = "{D71C2EF0-F15B-47C7-80E9-86D671F9C595}";
-plugin4.url                 = "speech/index.html";
-plugin4.icons               = ["speech/icon.png", "speech/icon@2x.png"];
-plugin4.isVisual            = false;
-plugin4.initDataType        = Asc.EPluginDataType.text;
-plugin4.isUpdateOleOnResize = false;
-plugin4.buttons             = [];
