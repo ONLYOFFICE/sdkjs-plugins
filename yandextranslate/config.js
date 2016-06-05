b645a7f568ac0549b6b607e728134abad738531a
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-06-05 14:00:52 +0300

    добавлен плагин-переводчик на русский со всех языков (яндекс-переводчик)

---
+++ yandextranslate/config.js
@@ -0,0 +1,26 @@
+window.Asc.plugin = {
+    "name" : "yandextranslate",
+    "guid" : "asc.{D3E759F7-3947-4BD6-B066-E184BBEDC675}",
+
+    "variations" : [
+        {
+            "description" : "yandextranslate",
+            "url"         : "yandextranslate/index.html",
+
+            "icons"           : ["yandextranslate/icon.png", "yandextranslate/icon@2x.png"],
+            "isViewer"        : false,
+            "EditorsSupport"  : ["word", "cell", "slide"],
+
+            "isVisual"        : false,
+            "isModal"         : false,
+            "isInsideMode"    : false,
+
+            "initDataType"    : "text",
+            "initData"        : "",
+
+            "isUpdateOleOnResize" : false,
+
+            "buttons"         : []
+        }
+    ]
+};
