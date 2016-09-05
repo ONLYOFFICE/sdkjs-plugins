12bed7a314ab9361e669a4e2869668f55e80738c
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-09-05 18:21:53 +0300

    .

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -5,7 +5,7 @@
         var sScript = 'var oDocument = Api.GetDocument();';
         sScript += 'oDocument.CreateNewHistoryPoint();';
         sScript += 'oParagraph = Api.CreateParagraph();';
-        sScript += 'oParagraph.AddText(\'Hello word!\');';
+        sScript += 'oParagraph.AddText(\'Hello world!\');';
         sScript += 'oDocument.InsertContent([oParagraph]);';
         window.Asc.plugin.info.recalculate = true;
         this.executeCommand("close", sScript);
