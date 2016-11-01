76bec4155d808672226db1d2ba25f3832e40d335
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-11-01 15:08:11 +0300

    example interface changer

--- interface/code.js
+++ interface/code.js
@@ -1,5 +1,18 @@
 (function() {
 
-	// code
+    $('#header').hide();
+    //$('#toolbar').hide();
+    $('#left-menu').hide();
+    $('#right-menu').hide();
+    $('#toolbar .toolbar').css('background-color','red');
+
+    var viewportController = DE.getController('Viewport');
+
+    if (viewportController) {
+        viewportController.onLayoutChanged('toolbar');
+        viewportController.onLayoutChanged('rightmenu');
+        viewportController.onLayoutChanged('leftmenu');
+        viewportController.onLayoutChanged();
+    }
 
 })();
