8dd80846a1736afe6a255ea833262f116c2073fe
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2016-11-16 14:03:20 +0300

    Interface. For changing interface before hiding preloader need to append path to plugin in config->editorConfig->plugins->UIpluginsData (e. g. UIpluginsData: ['interface/config.json']) and need to change config.json: parameter "isUICustomizer" must be true, parameter "url" is the path to customizing function.

--- interface/code.js
+++ interface/code.js
@@ -1,13 +1,17 @@
 (function() {
 
     $('#header').hide();
-    //$('#toolbar').hide();
     $('#left-menu').hide();
     $('#right-menu').hide();
     $('#toolbar .toolbar').css('background-color','red');
+    $('#toolbar #id-toolbar-full-placeholder-btn-settings').hide();
 
-    var viewportController = DE.getController('Viewport');
-
+    var viewportController;
+    try { viewportController = DE.getController('Viewport'); } catch(e) {
+         try { viewportController = PE.getController('Viewport'); } catch(e) {
+             try { viewportController =  SSE.getController('Viewport'); } catch(e) {}
+         }
+    }
     if (viewportController) {
         viewportController.onLayoutChanged('toolbar');
         viewportController.onLayoutChanged('rightmenu');
@@ -15,4 +19,10 @@
         viewportController.onLayoutChanged();
     }
 
+    var afterDocReady = function(){
+        $('#toolbar #id-toolbar-btn-hidebars > ul > li:nth-child(2)').hide();
+        $('#toolbar #id-toolbar-btn-showmode > ul > li:nth-child(2)').hide();
+    };
+
+    Common.NotificationCenter.on('document:ready', afterDocReady);
 })();
