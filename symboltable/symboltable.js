1a0bd214641ec787e89448f7975ef9e123eb6fb7
SergeyLuzyanin <Sergey.Luzyanin@onlyoffice.com>
2017-07-18 15:27:29 +0300

    save recents to local storage

--- symboltable/symboltable.js
+++ symboltable/symboltable.js
@@ -379,36 +379,11 @@
         oDiv.innerHTML = sInnerHtml;
     }
 
-
-
-    function getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
-
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires="+d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
-
     function fillRecentSymbols(){
-        var sCookie = getCookie('recent');
+        var sRecents = window.localStorage.getItem('recentSymbols');
         var aRecentCookies;
-        if(sCookie != ''){
-            aRecentCookies = JSON.parse(sCookie);
+        if(sRecents != ''){
+            aRecentCookies = JSON.parse(sRecents);
         }
         if(Array.isArray(aRecentCookies)){
             aRecents = aRecentCookies;
@@ -417,7 +392,7 @@
 
     function saveRecent(){
         var sJSON = JSON.stringify(aRecents);
-        setCookie('recent', sJSON, 360);
+        window.localStorage.setItem('recentSymbols', sJSON);
     }
 
 
