ee84b214047bc635fbf35013f2795bbfb9f68eee
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-08-07 14:44:46 +0300

    Fix plugin valgenesis.

--- plugin_valgenesis/code.js
+++ plugin_valgenesis/code.js
@@ -5,6 +5,11 @@
         (typeof (statusbarController.statusbar.mode.customization) == 'object') && !!statusbarController.statusbar.mode.customization.showReviewChanges) {
         var onReviewToggle = function(btn, state) {
             btn.toggle(false, true);
+            statusbarController.statusbar.mnuTrackChanges.setChecked(false, true);
+            if (statusbarController.api) {
+                statusbarController.api.asc_SetTrackRevisions(false);
+            }
+
             Common.NotificationCenter.trigger('edit:complete', statusbarController.statusbar);
         };
         statusbarController.statusbar.btnReview.on('toggle', onReviewToggle);
