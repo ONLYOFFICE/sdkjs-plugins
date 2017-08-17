d2a15b8c808f0891e79b45bbf67942e6bc741590
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2017-08-17 19:30:10 +0300

    .

--- examples/templates/templates.js
+++ examples/templates/templates.js
@@ -136,6 +136,15 @@
 			}
 		};
 		client.send();
-	}
+	};
+
+	window.Asc.plugin.onExternalMouseUp = function()
+    {
+        var evt = document.createEvent("MouseEvents");
+        evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
+            false, false, false, false, 0, null);
+
+        document.dispatchEvent(evt);
+    };
 
 })(window, undefined);
