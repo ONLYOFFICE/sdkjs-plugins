826fc9eabc71a9adc65a8b0390c5a07cef92ec8a
Alexey.Matveev <Alexey.Matveev@onlyoffice.com>
2017-12-27 13:33:10 +0300

    Changed the code highlighting script

--- code/code.js
+++ code/code.js
@@ -10,33 +10,20 @@
 	};
 	var language = hljs.listLanguages();	// array languages
 	var isInitLang = false; 	//flag init lang select
-
+	var language_select;		//select for languages
 
 	window.Asc.plugin.onMethodReturn = function(returnValue)
 	{
 	   $(document).ready(function () {
-	
-
-		  
-		  
-			
+			//to do	
 	   });
-		
 	}
-
-	
-	
-	
-	
-
 	window.Asc.plugin.init = function(text)
 	{		
-		var language_select = document.getElementById("language_id");
-		var style = document.getElementById("code_style");
+		language_select = document.getElementById("language_id");
 		var temp_code;
 		var curLang; 	//l
 		var flag = false;	//flag change code (true = changed)
-		style.href = "//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/github-gist.min.css"; //if you need change use -> .href = "highlight/styles/NameStyle.css";
 		if (!isInitLang)
 		{
 			initLang();
@@ -44,17 +31,15 @@
 		function initLang()
 		{
 			let temp_language = [];
-			for (var i = 0; i < language.length; i++)
+			for (let i = 0; i < language.length; i++)
 				{
 					temp_language += ("<option value=\"" + (i + 1) + "\">" + language[i] + "</option>");
 				}
-			
 			language_select.innerHTML ="<option value='" + 0 + "'>" + "Auto" + "</option>" + temp_language;
 			isInitLang = true;
 		}
 
 		curLang = language_select.options[language_select.selectedIndex].text;		//get current language
-		
 		language_select.onchange = function(e) { 								
 			let changeLang = language_select.options[language_select.selectedIndex].text;		// change current language
 			if (curLang != changeLang)
@@ -64,104 +49,109 @@
 			}
 				
 		 };
-		 if (!flag)
-		 {
-			 ChangeCode(curLang)
-		 }
-		 function ChangeCode(curLang) {
-		 	if (curLang == "Auto"){
-				temp_code = hljs.highlightAuto(text, language);
-				var temp_past = document.getElementById("code_id");
-				temp_past.innerHTML = "<pre>" + temp_code.value + "</pre>";
+			if (!flag)
+			{
+				ChangeCode(curLang)
 			}
-			else {
-				temp_code = hljs.highlight(curLang, text, true, 0);
-				var temp_past = document.getElementById("code_id");
-				//var finalStyle = document.defaultView.getComputedStyle(document.temp_past, "");
-				temp_past.innerHTML = "<pre>" + temp_code.value + "</pre>";
-				//_htmlPast = "<pre>" + temp_code.value + "</pre>";
-				/*_htmlPast = '<!DOCTYPE html>\
-								<html lang="en">\
-									<head>\
-										<meta charset="UTF-8"> \
-										<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github-gist.min.css"> \
-										<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>\
-										<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>\
-									</head>\
-										<body>\
-											<div id="mydiv">' + temp_code.value + '</div> \
-											<script>\
-												var text = $(\'#mydiv\').text();\
-												var language = hljs.listLanguages(); \
-												var temp_code = hljs.highlightAuto(text, language);\
-												document.body.innerHTML = "<pre>" + temp_code.value + "</pre>"\
-											</script>\
-										 </body>\
-								 </html>'*/
-								 _htmlPast = "<!DOCTYPE html> <html lang=\"en\"> <meta charset=\"UTF-8\"> <head>\
-								 <style>\
-										.hljs {\
-										display: block;\
-										background: white;\
-										padding: 0.5em;\
-										color: #333333;\
-										overflow-x: auto;}\
-										.hljs-comment,\
-										.hljs-meta {\
-										color: #969896;}\
-										.hljs-string,\
-										.hljs-variable,\
-										.hljs-template-variable,\
-										.hljs-strong,\
-										.hljs-emphasis,\
-										.hljs-quote {\
-										color: #df5000;}\
-										.hljs-keyword,\
-										.hljs-selector-tag,\
-										.hljs-type {\
-										color: #a71d5d;}\
-										.hljs-literal,\
-										.hljs-symbol,\
-										.hljs-bullet,\
-										.hljs-attribute {\
-										color: #0086b3;}\
-										.hljs-section,\
-										.hljs-name {\
-										color: #63a35c;}\
-										.hljs-tag {\
-										color: #333333;}\
-										.hljs-title,\
-										.hljs-attr,\
-										.hljs-selector-id,\
-										.hljs-selector-class,\
-										.hljs-selector-attr,\
-										.hljs-selector-pseudo {\
-										color: #795da3;}\
-										.hljs-addition {\
-										color: #55a532;\
-										background-color: #eaffea;}\
-										.hljs-deletion {\
-										color: #bd2c00;\
-										background-color: #ffecec;}\
-										.hljs-link {\
-										text-decoration: underline;}\
-									</style>\
-								 </head> <body font-size: 11px; font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;> <pre>" + temp_code.value + "</pre> </body> </html>"; 
-				window.Asc.plugin.executeMethod("PasteHtml", [_htmlPast]);
-				
-			}
-		 }
-
-		 
-
-		 //var temp_code = hljs.highlightAuto(text, language); //hljs.highlight("javascript", text, true, 0);
-		//_htmlPast = '<!DOCTYPE html> <html lang="en"> <meta charset="UTF-8"> <head> <link id="code_style" rel="stylesheet" href="https://github.com/isagalaev/highlight.js/tree/master/src/styles/default.css"> </head> <body>' +"<pre>" + temp_code.value + "</pre>" +'</body> </html>' 
-		//window.Asc.plugin.executeMethod("PasteHtml", [_htmlPast]);
-		
-	
-			
+			function ChangeCode(curLang) {
+				if ((curLang == "Auto") && text)
+				{
+					temp_code = hljs.highlightAuto(text, language);
+					createHTML(temp_code);
+					text="";
+				}
+				else if(text) 
+				{
+					temp_code = hljs.highlight(curLang, text, true, 0);
+					createHTML(temp_code);
+					text="";	
+				}
+		 	}
 	};
-	
+	function createHTML(code)
+	{
+		let tmp = code.value.trim();
+		for (let i=0;i<code.value.length;i++)
+		{
+			tmp = tmp.replace("\n","<br>");
+		}
+		//var temp_past = document.getElementById("code_id");
+		//temp_past.innerHTML = "<pre>" + tmp + "</pre>";
+		_htmlPast = "<!DOCTYPE html>\
+			<html lang=\"en\"> \
+				<head>\
+					<meta charset=\"UTF-8\"> \
+					<style>\
+					.hljs {\
+						display: block;\
+						overflow-x: auto;\
+						padding: 0.5em;\
+						background: white;\
+						color: black;}\
+					.hljs-comment,\
+					.hljs-quote {\
+						color: #800;}\
+					.hljs-keyword,\
+					.hljs-selector-tag,\
+					.hljs-section,\
+					.hljs-title,\
+					.hljs-name {\
+						color: #008;}\
+					.hljs-variable,\
+					.hljs-template-variable {\
+						color: #660;}\
+					.hljs-string,\
+					.hljs-selector-attr,\
+					.hljs-selector-pseudo,\
+					.hljs-regexp {\
+						color: #080;}\
+					.hljs-literal,\
+					.hljs-symbol,\
+					.hljs-bullet,\
+					.hljs-meta,\
+					.hljs-number,\
+					.hljs-link {\
+						color: #066;}\
+					.hljs-title,\
+					.hljs-doctag,\
+					.hljs-type,\
+					.hljs-attr,\
+					.hljs-built_in,\
+					.hljs-builtin-name,\
+					.hljs-params {\
+						color: #606;}\
+					.hljs-attribute,\
+					.hljs-subst {\
+						color: #000;}\
+					.hljs-formula {\
+						background-color: #eee;\
+						font-style: italic;}\
+					.hljs-selector-id,\
+					.hljs-selector-class {\
+						color: #9B703F;}\
+					.hljs-addition {\
+						background-color: #baeeba;}\
+					.hljs-deletion {\
+						background-color: #ffc8bd;}\
+					.hljs-doctag,\
+					.hljs-strong {\
+						font-weight: bold;}\
+					.hljs-emphasis {\
+						font-style: italic;}\
+					</style>\
+				</head> \
+				<body style = font-family: Consolas\"><pre>" + tmp.trim(); + "</pre></body>\
+			</html>"; 
+			window.Asc.plugin.executeMethod("PasteHtml", [_htmlPast]);
+			for (let i=0; i<language_select.length;i++)
+			{
+				if (language_select.options[i].text == code.language)
+				{
+					language_select.selectedIndex = i;
+				}	
+			}	
+
+	}
 	//close plugin
 	window.Asc.plugin.button = function(id)
 	{
