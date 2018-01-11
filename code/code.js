bb628b9c5034c5dfe943c120235d50d2e64aee98
Alexey.Matveev <Alexey.Matveev@onlyoffice.com>
2018-01-11 18:43:00 +0300

    changed work plugin code hightlight

--- code/code.js
+++ code/code.js
@@ -8,80 +8,164 @@
 			e.stopPropagation();
 		return false;
 	};
-	var language = hljs.listLanguages();	// array languages
-	var isInitLang = false; 	//flag init lang select
-	var language_select;		//select for languages
 
-	window.Asc.plugin.onMethodReturn = function(returnValue)
-	{
-	   $(document).ready(function () {
-			//to do	
-	   });
-	}
+	var language = hljs.listLanguages(),	// array languages
+		isInitLang = false, 				//flag init lang select
+		language_select,					//select for languages
+		_htmlPast,							//from paste in document
+		curLang,							//current language
+		code_field;							//field for higlight code
+		//для сохранения курсора
+		var selection,
+				range;//для сохранения курсора
+	
+	$(document).ready(function () {	
+		$("div").keydown(function(event){
+			// выходим если это не кропка tab
+			if( event.keyCode != 9 )
+				return;
+			event.preventDefault(); 
+			$(function () {
+				var $content = $('[data-target="insert"]');
+					$content.trigger('focus');
+					insertHTML("\t");
+			});
+		});
+	});
+	
 	window.Asc.plugin.init = function(text)
-	{		
+	{
+		code_field = document.getElementById("code_id");		
 		language_select = document.getElementById("language_id");
-		var temp_code;
-		var curLang; 	//l
-		var flag = false;	//flag change code (true = changed)
+		var temp_code,
+			flag = false;	//flag change code (true = changed)
 		if (!isInitLang)
 		{
 			initLang();
 		}
-		function initLang()
-		{
-			let temp_language = [];
-			for (let i = 0; i < language.length; i++)
-				{
-					temp_language += ("<option value=\"" + (i + 1) + "\">" + language[i] + "</option>");
-				}
-			language_select.innerHTML ="<option value='" + 0 + "'>" + "Auto" + "</option>" + temp_language;
-			isInitLang = true;
-		}
-
 		curLang = language_select.options[language_select.selectedIndex].text;		//get current language
-		language_select.onchange = function(e) { 								
-			let changeLang = language_select.options[language_select.selectedIndex].text;		// change current language
-			if (curLang != changeLang)
+		language_select.onchange = function(e) {
+			text = document.getElementById("code_id").innerText;				
+			curLang = language_select.options[language_select.selectedIndex].text;		// change current language
+				ChangeCode(curLang);
+				flag = true;	
+		};
+		if (!flag)
+		{
+			document.getElementById("code_id").focus();
+			ChangeCode(curLang);
+			//text = "";
+		}
+		function ChangeCode(curLang) {
+			if ((curLang == "Auto") && text)
 			{
-				ChangeCode(changeLang);
-				flag = true;
+				temp_code = hljs.highlightAuto(text, language);
+				createPreview(temp_code);
 			}
-				
-		 };
-			if (!flag)
+			else if(text) 
 			{
-				ChangeCode(curLang)
+				temp_code = hljs.highlight(curLang, text, true, 0);
+				createPreview(temp_code);	
 			}
-			function ChangeCode(curLang) {
-				if ((curLang == "Auto") && text)
-				{
-					temp_code = hljs.highlightAuto(text, language);
-					createHTML(temp_code);
-					text="";
-				}
-				else if(text) 
-				{
-					temp_code = hljs.highlight(curLang, text, true, 0);
-					createHTML(temp_code);
-					text="";	
-				}
-		 	}
+		};	
+		//document.getElementById("code_id").focus();
+
+		code_field.oninput = function() {
+			text = document.getElementById("code_id").innerText;
+			ChangeCode(curLang);
+		};
+
+		window.onresize = function()
+		{
+			//to update div
+			update();
+		};
+		window.Asc.plugin.resizeWindow(880, 600, 390, 400, 0, 0);				//resize plugin window		
 	};
+	function update()
+	{
+		//to do
+	};
+
+	function initLang()
+	{
+		let temp_language = [];
+		for (let i = 0; i < language.length; i++)
+			{
+				temp_language += ("<option value=\"" + (i + 1) + "\">" + language[i] + "</option>");
+			}
+		language_select.innerHTML ="<option value='" + 0 + "'>" + "Auto" + "</option>" + temp_language;
+		isInitLang = true;
+	};
+	
+	function insertHTML(html) {
+		try {
+			var selection = window.getSelection(),
+				range = selection.getRangeAt(0),
+				temp = document.createElement("div"),
+				insertion = document.createDocumentFragment();
+	
+			temp.innerHTML = html;
+	
+			while (temp.firstChild) {
+				insertion.appendChild(temp.firstChild);
+			}
+			//range.deleteContents();	//delete the value
+			range.insertNode(insertion);
+		} catch (z) {
+			try {
+				document.selection.createRange().pasteHTML(html);
+			} catch (z) {}
+		}
+	}
+
+	function createPreview(code)
+	{
+		
+			//var selection = window.getSelection(),
+				//range = selection.getRangeAt(0),
+				//insertion = document.createDocumentFragment();
+			//range.deleteContents();	//delete the value
+			//range.insertNode(insertion);
+
+				//selection = window.getSelection();
+				//range = selection.getRangeAt(0);
+				//insertion = document.createDocumentFragment();
+				//selection.removeAllRanges();
+				//selection.addRange(range);
+
+				//sel = window.getSelection();
+				//sel.removeAllRanges();
+				//sel.addRange(range);
+				
+				code_field.innerHTML = code.value;
+				
+				
+				
+
+		for (let i=0; i<language_select.length;i++)
+		{
+			if (language_select.options[i].text == code.language)
+			{
+				curLang = code.language;
+				language_select.selectedIndex = i;
+			}	
+		}	
+	};
+	
 	function createHTML(code)
 	{
-		let tmp = code.value.trim();
-		for (let i=0;i<code.value.length;i++)
+		for (let i=0;i<code.length;i++)
 		{
-			tmp = tmp.replace("\n","<br>");
+			code = code.replace("\n","<br>");
+			code = code.replace("\t","&emsp;&emsp;");
 		}
-		//var temp_past = document.getElementById("code_id");
-		//temp_past.innerHTML = "<pre>" + tmp + "</pre>";
 		_htmlPast = "<!DOCTYPE html>\
 			<html lang=\"en\"> \
 				<head>\
 					<meta charset=\"UTF-8\"> \
 					<style>\
+					<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">\
 					.hljs {\
 						display: block;\
 						overflow-x: auto;\
@@ -140,22 +224,22 @@
 						font-style: italic;}\
 					</style>\
 				</head> \
-				<body style = font-family: Consolas\"><pre>" + tmp.trim(); + "</pre></body>\
+				<body style = white-space: pre; font-family: Consolas\">" + code.trim(); + "</body>\
 			</html>"; 
-			window.Asc.plugin.executeMethod("PasteHtml", [_htmlPast]);
-			for (let i=0; i<language_select.length;i++)
-			{
-				if (language_select.options[i].text == code.language)
-				{
-					language_select.selectedIndex = i;
-				}	
-			}	
+	};
 
-	}
-	//close plugin
 	window.Asc.plugin.button = function(id)
 	{
-		this.executeCommand("close", "");
+		if(id==0)
+		{
+			createHTML(document.getElementById("code_id").innerHTML);
+			window.Asc.plugin.executeMethod("PasteHtml", [_htmlPast]);
+			this.executeCommand("close", "");
+		}
+		if((id==-1) || (id==1))
+		{
+			this.executeCommand("close", "");
+		}
 	};
 
 	window.Asc.plugin.onExternalMouseUp = function()
@@ -163,8 +247,7 @@
 		var evt = document.createEvent("MouseEvents");
 		evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
 			false, false, false, false, 0, null);
-
 		document.dispatchEvent(evt);
 	};
-		  
+
 })(window, undefined);
