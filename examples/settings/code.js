51dc70a776f09342e3f7de2b8b02aacdfd517932
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2019-01-29 15:24:07 +0300

    .

--- examples/settings/code.js
+++ examples/settings/code.js
@@ -4,7 +4,42 @@
     window.Asc.plugin.init = function()
     {
 		var initSettings = {
-			copyoutenabled : false
+			copyoutenabled : false,
+			watermark_on_draw : JSON.stringify({
+				"transparent" : 0.3,
+				"type" : "rect",
+				"width" : 100,
+				"height" : 100,
+				"rotate" : -45,
+				"margins" : [ 10, 10, 10, 10 ],
+				"fill" : [255, 0, 0],
+				"stroke-width" : 1,
+				"stroke" : [0, 0, 255],
+				"align" : 1,
+				
+				"paragraphs" : [ {
+					"align" : 2,
+					"fill" : [255, 0, 0],
+					"linespacing" : 1,
+					
+					"runs" : [
+							{
+								"text" : "Do not steal, %user_name%!",
+								"fill" : [0, 0, 0],
+								"font-family" : "Arial",
+								"font-size" : 40,
+								"bold" : true,
+								"italic" : false,
+								"strikeout" : false,
+								"underline" : false
+							},
+							{
+								"text" : "<%br%>"
+							}
+						]
+					}
+				]
+			})
 		};
 		
         this.executeMethod("SetProperties", [initSettings], function() {
