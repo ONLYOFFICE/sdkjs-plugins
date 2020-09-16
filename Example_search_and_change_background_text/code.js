4a7460932a63a7882f80a7713a0c9bb6a828a6e7
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-09-16 17:51:24 +0300

    Changed plugin example_search_and_change_background_text

--- Example_search_and_change_background_text/code.js
+++ Example_search_and_change_background_text/code.js
@@ -6,28 +6,58 @@
 		$('#btn_prev').on('click', function() {
 			var text = $('#inp_search').val();
 			window.Asc.plugin.executeMethod("SearchText",[text, false]);
-			
 		});
 
 		$('#btn_next').on('click', function() {
 			var text = $('#inp_search').val();
 			window.Asc.plugin.executeMethod("SearchText",[text, true]);
-			
 		});
 
 		$('#btn_bg').on('click', function() {
-			var color = $("#inp_color").val();
-			window.Asc.plugin.executeMethod("SetHighlightText",[color]);
+			var strcolor = $("#inp_color").val();
+			var r = parseInt(strcolor[1] + strcolor[2], 16),
+                g = parseInt(strcolor[3] + strcolor[4], 16),
+				b = parseInt(strcolor[5] + strcolor[6], 16);
+			Asc.scope.color = {
+				r : r,
+				g : g,
+				b : b,
+				isNone : false
+			};
+			window.Asc.plugin.callCommand(function() {
+				var oDocument = Api.GetDocument();
+				var range = oDocument.GetRangeBySelect();
+				range.SetHighlight(Asc.scope.color.r, Asc.scope.color.g, Asc.scope.color.b, Asc.scope.color.isNone);
+			}, false, true);
 		});
 
 		$('#btn_bgAll').on('click', function() {
-			var color = $("#inp_color").val();
-			var text = $('#inp_search').val();
-			window.Asc.plugin.executeMethod("SetHighlightSearched",[text,color]);
+			var strcolor = $("#inp_color").val();
+			var r = parseInt(strcolor[1] + strcolor[2], 16),
+                g = parseInt(strcolor[3] + strcolor[4], 16),
+				b = parseInt(strcolor[5] + strcolor[6], 16);
+			Asc.scope.color = {
+				text : $('#inp_search').val(),
+				r : r,
+				g : g,
+				b : b,
+				isNone : false
+			};
+			window.Asc.plugin.callCommand(function() {
+				var oDocument = Api.GetDocument();
+				var arrRanges = oDocument.Search(Asc.scope.color.text, false);
+				for (var i = 0; i < arrRanges.length; i++) {
+					arrRanges[i].SetHighlight(Asc.scope.color.r, Asc.scope.color.g, Asc.scope.color.b, Asc.scope.color.isNone);
+				}
+			}, false, true);
 		});
 
 		$('#btn_resetColor').on('click', function() {
-			window.Asc.plugin.executeMethod("SetHighlightText",["none"]);
+			window.Asc.plugin.callCommand(function() {
+				var oDocument = Api.GetDocument();
+				var range = oDocument.GetRangeBySelect();
+				range.SetHighlight(null, null, null, true);
+			}, false, true);
 		});
 		$( "#inp_search" ).keydown(function( event ) {
 			if ( event.which == 13 ) {
@@ -40,6 +70,5 @@
     {
 		this.executeCommand("close", "");
 	};
-
 	
 })(window, undefined);
