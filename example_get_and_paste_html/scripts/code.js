3c45798e6744ce9a00fed5b3f90df7c308fdb2e7
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-11-25 13:50:31 +0300

    Add formatting html in plugin example_get_and_paste_html.

--- example_get_and_paste_html/scripts/code.js
+++ example_get_and_paste_html/scripts/code.js
@@ -3,6 +3,13 @@
 	var editor;
 	window.Asc.plugin.init = function(text)
 	{
+		// text = text.replace(/\<[\/]?p[a-zA-Z0-9-:;+"\/= >.]*/g,"");
+		text = text.replace(/class="[a-zA-Z0-9-:;+"\/=]*/g,"");
+		text = text.replace(/\<p/g,"\r\n\t\t<p");
+		text = text.replace(/\<img/g,"\r\n\t\t<img");
+		var temp = (text.indexOf("<p") === -1) ? "\r\n" : ""
+		if (text !== "")
+			text = "<html>\r\n\t<body>" + temp + "\t\t" + text + '\r\n\t</body>\r\n</html>';
 		document.getElementById("div_main").style.width = document.getElementById("body").clientWidth- 20 +"px";
 		if (editor) {
 			editor.setValue(text);
@@ -19,7 +26,7 @@
 			editor = CodeMirror.fromTextArea(document.getElementById("main"), {
 				mode: "text/html",
 				lineNumbers: true,
-				lineWrapping: true
+				lineWrapping: false
 			});
 		}
 		
