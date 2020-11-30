3785ea1f0f190421dec139eab3c00d9c6ffc234e
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-11-30 12:19:50 +0300

    Fix bug #47737

--- example_count_words_and_characters/scripts/code.js
+++ example_count_words_and_characters/scripts/code.js
@@ -3,12 +3,11 @@
 	
 	window.Asc.plugin.init = function(text)
 	{
-		var chars = text.replace(/\r*\n/g, '').length;
 		var words = (text.match(/\S+/g)) ? text.match(/\S+/g).length : 0;
-		document.getElementById("charsNoSpaces").innerHTML = `Сharacters without spaces: ${text.replace(/\s+/g, '').length}`;
-		document.getElementById("chars").innerHTML         = `Total characters: ${chars}`;
-		document.getElementById("words").innerHTML         = `Words count: ${words}`;
-		document.getElementById("lines").innerHTML         = `Lines count: ${text.split(/\r*\n/).length}`;
+		document.getElementById("charsNoSpaces").innerHTML = "Сharacters without spaces: " + text.replace(/\s+/g, '').length;
+		document.getElementById("chars").innerHTML         = "Total characters: " + text.replace(/\r*\n/g, '').length;
+		document.getElementById("words").innerHTML         = "Words count: " + words;
+		document.getElementById("lines").innerHTML         = "Lines count: " + text.split(/\r*\n/).length;
 		
 	};
 
