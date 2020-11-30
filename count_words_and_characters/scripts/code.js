db06d346271133a8a9a03631c425d7efa6d71f10
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-11-30 16:42:34 +0300

    Renamed plugin example_count_words_and_characters --> count_words_and_characters

---
+++ count_words_and_characters/scripts/code.js
@@ -0,0 +1,23 @@
+(function(window, undefined)
+{
+	
+	window.Asc.plugin.init = function(text)
+	{
+		var chars = text.replace(/\r*\n/g, '').length;
+		var words = (text.match(/\S+/g)) ? text.match(/\S+/g).length : 0;
+		var lines = text.split(/\r\n/);
+		if (lines[lines.length-1] == "") lines.length--;
+		document.getElementById("charsNoSpaces").innerHTML = "Characters without spaces: " + text.replace(/\s+/g, '').length;
+		document.getElementById("chars").innerHTML         = "Total characters: " + chars;
+		document.getElementById("words").innerHTML         = "Words count: " + words;
+		document.getElementById("lines").innerHTML         = "Paragraphs count: " + lines.length;
+		
+	};
+
+
+	window.Asc.plugin.button = function()
+	{
+		this.executeCommand("close", "");
+	};
+
+})(window, undefined);
