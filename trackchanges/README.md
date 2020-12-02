4346324b8ff8bb01a7b2b5bf14d67d947d56a98e
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-12-02 12:03:00 +0300

    Changed plugin trackchanges

--- trackchanges/README.md
+++ trackchanges/README.md
@@ -4,4 +4,8 @@ This plugin enables track changes in any document and does not allow you to disa
 
 It is without interface plugin and isn't installed by default in cloud, [self-hosted](https://github.com/ONLYOFFICE/DocumentServer) and [desktop version](https://github.com/ONLYOFFICE/DesktopEditors) of ONLYOFFICE editors. 
 
+## How to use
+
+1. Add this plugin to yourself and open document editor.
+
 If you need more information about how to use or write your own plugin, please see this https://api.onlyoffice.com/plugin/basic
---
+++ trackchanges/scripts/code.js
@@ -0,0 +1,15 @@
+(function() {
+
+    // var afterDocReady = function(){
+    var reviewController;
+    try { reviewController = DE.getController('Common.Controllers.ReviewChanges'); } catch(e) {}
+    if (reviewController && reviewController.view && reviewController.view.btnsTurnReview) {
+        reviewController.view.btnsTurnReview.forEach(function(button) {
+            button.allowDepress = false;
+        }, this);
+        Common.NotificationCenter.trigger('reviewchanges:turn', 'on');
+    }
+    // };
+
+    // Common.NotificationCenter.on('document:ready', afterDocReady);
+})();
