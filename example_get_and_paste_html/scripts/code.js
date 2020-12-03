cc1d999c680eb4a76d5a4bb5f1a83d2eb2a67321
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-12-03 15:28:51 +0300

    Changed plugin example_get_and_paste_html (changed work with codemirror editor)

--- example_get_and_paste_html/scripts/code.js
+++ example_get_and_paste_html/scripts/code.js
@@ -13,9 +13,10 @@
 		document.getElementById("div_main").style.width = document.getElementById("body").clientWidth- 20 +"px";
 		if (editor) {
 			editor.setValue(text);
-		} else {
-			document.getElementById("main").value = text;
-		}
+		} 
+		// else {
+			// document.getElementById("main").value = text;
+		// }
 		window.onresize = function(e){
 			document.getElementById("div_main").style.width = document.getElementById("body").clientWidth- 20 +"px";
 		}
@@ -23,11 +24,17 @@
 			window.Asc.plugin.executeMethod("PasteHtml",[editor.getValue()]);
 		};
 		if (!editor) {
-			editor = CodeMirror.fromTextArea(document.getElementById("main"), {
+			editor = CodeMirror(document.getElementById("main"), {
 				mode: "text/html",
+				value: text,
 				lineNumbers: true,
 				lineWrapping: false
 			});
+			// editor = CodeMirror.fromTextArea(document.getElementById("main"), {
+			// 	mode: "text/html",
+			// 	lineNumbers: true,
+			// 	lineWrapping: false
+			// });
 		}
 		
 	};
