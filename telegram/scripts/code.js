d73c8fe015e5efd90c5ffc489ad4563dee8adb39
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-12-03 16:18:25 +0300

    Changed telegram plugin (added message about browser's incognito mode).

--- telegram/scripts/code.js
+++ telegram/scripts/code.js
@@ -1,7 +1,17 @@
 (function(window, undefined){
 
-    window.Asc.plugin.init = function(text)
+    window.Asc.plugin.init = function()
     {
+        //check incognito mode
+        var fs = window.RequestFileSystem || window.webkitRequestFileSystem;
+        if (fs) {
+            fs(window.TEMPORARY, 100, function(fs) {
+                document.getElementById("iframe").style.display = "block";
+            }, function(err) {
+                document.getElementById("result").style.display = "block";
+                document.getElementById("iframe").style.display = "none";
+            });
+        } 
     };
 
     window.Asc.plugin.button = function(id)
